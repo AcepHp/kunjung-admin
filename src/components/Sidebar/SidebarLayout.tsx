9e01a88d2919840f8a9277f22c53f387a15151f9
AcepHp <acephandika27@gmail.com>
2025-12-10 09:37:04 +0700

    fix: fix slicing login & sidebar

--- src/components/Sidebar/SidebarLayout.tsx
+++ src/components/Sidebar/SidebarLayout.tsx
@@ -429,13 +429,13 @@ export default function SidebarLayout({
                                 </MenuButton>
                                 <MenuItems
                                     transition
-                                    className="absolute right-0 z-10 mt-2.5 w-36 origin-top-right rounded-md bg-white py-2 ring-1 shadow-lg ring-gray-900/5 transition focus:outline-hidden data-closed:scale-95 data-closed:transform data-closed:opacity-0 data-enter:duration-100 data-enter:ease-out data-leave:duration-75 data-leave:ease-in"
+                                    className="absolute right-0 z-10 mt-2.5 w-40 origin-top-right rounded-md bg-white ring-1 shadow-lg ring-gray-900/5 transition focus:outline-hidden data-closed:scale-95 data-closed:transform data-closed:opacity-0 data-enter:duration-100 data-enter:ease-out data-leave:duration-75 data-leave:ease-in"
                                 >
                                     {userNavigation.map((item) => (
                                         <MenuItem key={item.name}>
                                             <a
                                                 href={item.href}
-                                                className="block px-3 py-1 text-sm/6 text-gray-900 data-focus:bg-gray-50 data-focus:outline-hidden"
+                                                className="block px-5 py-3 text-sm/6 text-gray-900 data-focus:bg-gray-50 data-focus:rounded-md data-focus:outline-hidden"
                                             >
                                                 {item.name}
                                             </a>
